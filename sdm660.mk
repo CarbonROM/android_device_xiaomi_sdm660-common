5b07b1187c56bde50a12edb3f7f1edf7305e8d5f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-07 08:29:36 +0000

    sdm660: Build safailnet
    Change-Id: I00fdf5579e8129801b9e74ffd5ce8661ff6d25ec

--- sdm660.mk
+++ sdm660.mk
@@ -263,6 +263,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.qti.fm.sh \
+    init.safailnet.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
