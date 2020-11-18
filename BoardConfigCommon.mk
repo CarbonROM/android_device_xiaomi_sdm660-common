2c6751daaa43c26377cf0f8cb16c7d4524a1900b
z3nitsu <lovepreet.11908810@lpu.in>
2020-11-18 19:20:05 +0530

    sdm660-common:rootdir:Decommonise fstab for lavender
    remove fstab.qcom conflit
    
    Change-Id: Ib4a29fe28cd1b056ab5fbd3187d6d497f003cc1b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,9 +220,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_QCOM_BSP := false
 
 # Recovery
-ifneq ($(filter lavender,$(TARGET_DEVICE)),)
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_A.qcom
-else ifeq ($(ENABLE_AB), true)
+ifeq ($(AB_OTA_UPDATER), true)
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_AB.qcom
 else
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
